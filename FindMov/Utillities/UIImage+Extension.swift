eb2bbe0bfe737c78f367072b02a924c8f277ff1e
Rathish Kannan <rathish.k@sunandsandsports.com>
2018-09-23 17:31:03 +0400

    Extension to download images

---
+++ FindMov/Utillities/UIImage+Extension.swift
@@ -0,0 +1,26 @@
+//----------------------------------------------------------------------------------
+//  File Name         :  UISearchBar+Extension.swift
+//  Description       :  Extension for UISearchBar
+//                       1. Manages -> Search Bar Appearance
+//  Author            :  Rathish Kannan
+//  E-mail            :  rathishnk@hotmail.co.in
+//  Dated             :  23rd Sep 2018
+//  Copyright (c) 2018 Rathish Kannan. All rights reserved.
+//-----------------------------------------------------------------------------------
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    convenience init?(url: URL?) {
+        guard let url = url else { return nil }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            self.init(data: data)
+        } catch {
+            print("Cannot load image from url: \(url) with error: \(error)")
+            return nil
+        }
+    }
+}
