80f3684ed9559b48183bf3a60b9231f4c0e1d3ac
Rathish Kannan <rathish.k@sunandsandsports.com>
2018-09-23 16:46:44 +0400

    Tableview Setup: Add tableview for viewing results

--- FindMov/Utillities/UISearchBar+Extension.swift
+++ FindMov/Utillities/UISearchBar+Extension.swift
@@ -1,10 +1,12 @@
-//
-//  UISearchBar+Extension.swift
-//  FindMov
-//
-//  Created by Rathish Kannan on 9/20/18.
-//  Copyright © 2018 Rathish Kannan. All rights reserved.
-//
+//----------------------------------------------------------------------------------
+//  File Name         :  UISearchBar+Extension.swift
+//  Description       :  Extension for UISearchBar
+//                       1. Manages -> Search Bar Appearance
+//  Author            :  Rathish Kannan
+//  E-mail            :  rathishnk@hotmail.co.in
+//  Dated             :  23rd Sep 2018
+//  Copyright (c) 2018 Rathish Kannan. All rights reserved.
+//-----------------------------------------------------------------------------------
 
 import Foundation
 import UIKit
