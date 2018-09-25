7a6de860614684bb63b8c322ab8a1d3a7b0f7aab
Rathish Kannan <rathish.k@sunandsandsports.com>
2018-09-25 16:26:22 +0400

    Handle infinte scrolling pagination

--- FindMov/ViewController.swift
+++ FindMov/ViewController.swift
@@ -13,7 +13,7 @@ import UIKit
 class ViewController: UIViewController {
     var searchBar: UISearchBar {
         guard let sBar = navigationItem.titleView as? UISearchBar else {
-            assertionFailure("UINavigationController not embedded!")
+            assertionFailure("NavigationController not embedded!")
             return UISearchBar()
         }
         sBar.backgroundColor           = .black
@@ -31,6 +31,10 @@ class ViewController: UIViewController {
     
     fileprivate let viewModel = MovieViewModel()
 
+    let manager = SearchManager()
+    
+    var param = SearchQuery(query: "", page: "", id: "", count: "")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         _ = searchBar
@@ -38,7 +42,6 @@ class ViewController: UIViewController {
         tableView?.estimatedRowHeight = 100
         tableView?.rowHeight = UITableViewAutomaticDimension
         tableView?.register(NamePictureCell.nib, forCellReuseIdentifier: NamePictureCell.identifier)
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,6 +51,9 @@ class ViewController: UIViewController {
 
 
 extension ViewController: UISearchBarDelegate {
+    /// Requirement(4)
+    ///
+    /// - Parameter searchBar: UISearchBar
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchBar.setShowsCancelButton(true, animated: true)
         searchBar.cancelButton?.setTitle("CANCEL", for: .normal)
@@ -59,19 +65,25 @@ extension ViewController: UISearchBarDelegate {
         }
     }
     
+    /// Search with Query from User
+    ///
+    /// - Parameter searchBar: UISearchBar
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.setShowsCancelButton(false, animated: true)
         searchBar.resignFirstResponder()
         
-        let manager = SearchManager()
         manager.searchMovies(query: searchBar.text ?? "", page: ""){  (results) in
             DispatchQueue.main.async {
-                self.viewModel.setUpData(results: results, forQuery: searchBar.text ?? "")
+                self.param = SearchQuery(query: searchBar.text ?? "", page: String(results?.page ?? 1), id: searchBar.text ?? "", count: String(results?.total_pages ?? 1))
+                self.viewModel.setUpData(results: results?.results ?? [], forQuery: searchBar.text ?? "")
                 self.tableView.reloadData()
             }
         }
     }
     
+    /// When user cancels the current search
+    ///
+    /// - Parameter searchBar: UISearchBar
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.setShowsCancelButton(false, animated: true)
         searchBar.resignFirstResponder()
@@ -85,3 +97,47 @@ extension ViewController: UISearchBarDelegate {
     }
 }
 
+extension ViewController: UITableViewDelegate {
+    /// Selection Support
+    ///
+    /// - Requirement(5):
+    ///   - tableView: UITableView
+    ///   - indexPath: IndexPath
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        searchBar.setShowsCancelButton(false, animated: true)
+        searchBar.resignFirstResponder()
+        let item = viewModel.items[indexPath.section]
+        if item.type == .recentItems{
+            manager.searchMovies(query: item.sectionTitle, page: ""){  (results) in
+                self.param = SearchQuery(query: item.sectionTitle, page: String(results?.page ?? 1), id: item.sectionTitle, count: String(results?.total_pages ?? 1))
+                DispatchQueue.main.async {
+                    self.viewModel.setUpData(results: results?.results ?? [], forQuery: item.sectionTitle)
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    /// Pagination Support
+    ///
+    /// - Requirement(2):
+    ///   - details: UIScrollView
+    ///   - decelerate: Bool
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let currentOffset = scrollView.contentOffset.y
+        let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height
+        if maximumOffset - currentOffset <= -0.5 && param.count > param.page {
+            let increment = Int(param.page) ?? 1
+            param.page = String(increment + 1)
+            manager.searchMovies(query: param.query, page: String(param.page)){  (results) in
+                DispatchQueue.main.async {
+                    self.viewModel.setUpData(results: results?.results ?? [], forQuery: self.param.query)
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+}
